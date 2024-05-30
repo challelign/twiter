ca2d8d53179e38ae1d659f1cde2d655fd91b2a01
Challelign Tilahun <chalie2123@gmail.com>
2024-05-30 09:10:18 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		// Enable the experimental features necessary for dynamic API routes
-		runtime: "nodejs", // or 'edge' if you are using edge functions
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
